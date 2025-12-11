5d0bd614f45810440b004fa5cb2d84341dc1a1a8
vchernov <vitaly.chernov@rackroom.com>
2025-12-10 23:44:35 -0500

    adding form; saving data

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId;
 
 /* page */
 const page = {
@@ -79,6 +80,7 @@ function rerenderContent(activeHabbit) {
 }
 
 function rerender(activeHabbitId) {
+    globalActiveHabbitId = activeHabbitId;
     const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
     if (!activeHabbit) {
         return;
@@ -86,6 +88,30 @@ function rerender(activeHabbitId) {
     rerenderMenu(activeHabbit);
     rerenderHead(activeHabbit);
     rerenderContent(activeHabbit);
+}
+
+/* work with days */
+function addDays(event) {
+    const form = event.target;
+    event.preventDefault();
+    const data = new FormData(form);
+    const comment = data.get('comment');
+    form['comment'].classList.remove('error');
+    if (!comment) {
+        form['comment'].classList.add('error');
+    }
+    habbits = habbits.map(habbit => {
+        if (habbit.id === globalActiveHabbitId) {
+            return {
+                ...habbit,
+                days: habbit.days.concat([{ comment }])
+            }
+        }
+        return habbit;
+    });
+    form['comment'].value = '';
+    rerender(globalActiveHabbitId);
+    saveData();
 }
 
 /* init */
